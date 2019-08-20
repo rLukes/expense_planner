4cf52f6804cd479ea50952a8ae37f34e56fdd780
Ruben <lukesruben@gmail.com>
2019-08-20 11:37:40 +0200

    added transaction model

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,45 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(ExpensePlannerApp());
 
-class ExpensePlannerApp extends StatelessWidget{
+class ExpensePlannerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   return MaterialApp(
-     title: 'Expense planner',
-     home: ExpensePlannerHomePage(),
-   );
+    return MaterialApp(
+      title: 'Expense planner',
+      home: ExpensePlannerHomePage(),
+    );
   }
 }
 
-
 class ExpensePlannerHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Expense planner'),),
-      body: Center(
-        child: Text('Hello'),
+      appBar: AppBar(
+        title: Text('Expense planner'),
       ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.deepOrangeAccent,
+              child: Text('Chart'),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            color: Colors.deepOrangeAccent,
+           child: Text("list"),
+          )
+        ],
+      )
+
     );
   }
 }
