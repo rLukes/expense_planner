0d79016203f2656b17451b9a64b6e5911348ee07
Ruben <lukesruben@gmail.com>
2019-08-20 12:15:21 +0200

    added some style to the widgets

--- lib/main.dart
+++ lib/main.dart
@@ -14,32 +14,58 @@ class ExpensePlannerApp extends StatelessWidget {
 }
 
 class ExpensePlannerHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [];
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: "new shoes", amount: 20.0, date: DateTime.now()),
+    Transaction(id: 't2', title: "Pc", amount: 40.0, date: DateTime.now())
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Expense planner'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            child: Card(
-              color: Colors.deepOrangeAccent,
-              child: Text('Chart'),
-              elevation: 5,
+        appBar: AppBar(
+          title: Text('Expense planner'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.deepOrangeAccent,
+                child: Text('Chart'),
+                elevation: 5,
+              ),
             ),
-          ),
-          Card(
-            color: Colors.deepOrangeAccent,
-           child: Text("list"),
-          )
-        ],
-      )
-
-    );
+            Card(
+              child: Column(
+                  children: transactions.map((tx) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        child: Text(
+                          tx.amount.toString(),
+                          style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20, color: Colors.purple),
+                        ),
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.purple, width: 2,)),
+                        padding: EdgeInsets.all(10),
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Text(tx.title),
+                          Text(tx.date.toString())
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList()),
+            )
+          ],
+        ));
   }
 }
