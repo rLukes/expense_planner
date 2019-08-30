6a1094f27228257f4e081546f50d2460c7d92a81
Ruben <lukesruben@gmail.com>
2019-08-30 09:47:34 +0200

    dateformat

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(ExpensePlannerApp());
 
@@ -45,19 +46,33 @@ class ExpensePlannerHomePage extends StatelessWidget {
                     children: <Widget>[
                       Container(
                         child: Text(
-                          tx.amount.toString(),
-                          style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20, color: Colors.purple),
+                          '\$${tx.amount}',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple),
                         ),
                         margin:
                             EdgeInsets.symmetric(vertical: 10, horizontal: 15),
                         decoration: BoxDecoration(
-                            border: Border.all(color: Colors.purple, width: 2,)),
+                            border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        )),
                         padding: EdgeInsets.all(10),
                       ),
                       Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
-                          Text(tx.title),
-                          Text(tx.date.toString())
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            DateFormat("dd-MM-yyyy").format(tx.date),
+                            style: TextStyle(color: Colors.grey),
+                          )
                         ],
                       )
                     ],
