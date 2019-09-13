19dd389311a6528892b1470de4477f782f1dfc8e
Ruben <lukesruben@gmail.com>
2019-09-13 15:19:21 +0200

    added datepicker

--- lib/widgets/transactions_list.dart
+++ lib/widgets/transactions_list.dart
@@ -19,7 +19,9 @@ class Transactions extends StatelessWidget {
                   "No transactions added yet!",
                   style: Theme.of(context).textTheme.title,
                 ),
-                SizedBox(height: 10,),
+                SizedBox(
+                  height: 10,
+                ),
                 Container(
                   child: Image.asset(
                     "assets/images/waiting.png",
@@ -31,40 +33,20 @@ class Transactions extends StatelessWidget {
             )
           : ListView.builder(
               itemBuilder: (ctx, index) {
-                var tx = this.trans[index];
                 return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        child: Text(
-                          '\$${tx.amount}',
-                          style: Theme.of(context).textTheme.title,
-                        ),
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                          color: Theme.of(context).primaryColor,
-                          width: 2,
-                        )),
-                        padding: EdgeInsets.all(10),
+                  elevation: 6,
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: FittedBox(child: Text('\$${trans[index].amount}')),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            tx.title,
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            DateFormat("dd-MM-yyyy").format(tx.date),
-                            style: TextStyle(
-                                color: Theme.of(context).primaryColor),
-                          )
-                        ],
-                      )
-                    ],
+                    ),
+                    title: Text(trans[index].title, style: Theme.of(context).textTheme.title,),
+                    subtitle: Text(DateFormat.yMMMd().format(trans[index].date)),
+
                   ),
                 );
               },
